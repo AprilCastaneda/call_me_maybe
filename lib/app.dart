dea3ae72578bbf41568dd94b1015658c26d960a9
April Castaneda <castanap@oregonstate.edu>
2021-02-13 19:02:22 -0800

    Added tabs/icons/screens

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,14 @@
-//import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'screens/main_screen.dart';
-import 'screens/alpha_screen.dart';
-import 'screens/beta_screen.dart';
-import 'screens/chi_screen.dart';
+// import 'screens/main_screen.dart';
+import 'screens/business_card_screen.dart';
+import 'screens/resume_screen.dart';
+import 'screens/predictor_screen.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Call Me Maybe',
         theme: ThemeData(primarySwatch: Colors.blue),
         home: MainTabController());
   }
@@ -18,36 +16,39 @@ class App extends StatelessWidget {
 
 class MainTabController extends StatelessWidget {
   static const tabs = [
-    Tab(text: 'Alpha'),
-    Tab(text: 'Beta'),
-    Tab(text: 'Chi'),
+    // Tab(icon: Icon(Icons.face)),
+    Tab(icon: Icon(Icons.pets)),
+    Tab(icon: Icon(Icons.assignment)),
+    Tab(icon: Icon(Icons.help_outline)),
+    // Tab(text: 'Business Card'),
+    // Tab(text: 'Resume'),
+    // Tab(text: 'Predictor'),
   ];
 
   final screens = [
-    AlphaScreen(),
-    BetaScreen(),
-    ChiScreen(),
+    BusinessCardScreen(),
+    ResumeScreen(),
+    PredictorScreen(),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Week 5 Explorations',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MainScreen(title: 'Explorations'),
-    );
-
-    // return DefaultTabController(
-    //   length: tabs.length,
-    //   initialIndex: 0,
-    //   child: Scaffold(
-    //     appBar: AppBar(
-    //         title: Text('Easy Tabs Example'), bottom: TabBar(tabs: tabs)),
-
-    //     //body: TabBarView(children: screens),
+    // return MaterialApp(
+    //   title: 'Week 5 Explorations',
+    //   theme: ThemeData(
+    //     primarySwatch: Colors.blue,
     //   ),
+    //   home: MainScreen(title: 'Call Me Maybe'),
     // );
+
+    return DefaultTabController(
+      length: tabs.length,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar:
+            AppBar(title: Text('Call Me Maybe'), bottom: TabBar(tabs: tabs)),
+        body: TabBarView(children: screens),
+      ),
+    );
   }
 }
