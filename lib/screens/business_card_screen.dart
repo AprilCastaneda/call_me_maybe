2f98a0ab7d7210047927eee82f11fa0df29f8f81
April Castaneda <castanap@oregonstate.edu>
2021-02-13 19:48:16 -0800

    Aligned BusinessCard screen to center

--- lib/screens/business_card_screen.dart
+++ lib/screens/business_card_screen.dart
@@ -1,10 +1,77 @@
 import 'package:flutter/material.dart';
 
 class BusinessCardScreen extends StatelessWidget {
+  // final String title;
+
+  // const BusinessCardScreen({Key key, this.title}) : super(key: key);
+
+  double paddingHorizontal(BuildContext context) {
+    return MediaQuery.of(context).size.width * 0.05;
+  }
+
+  double paddingVertical(BuildContext context) {
+    return MediaQuery.of(context).size.height * 0.05;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Business Card'),
-    );
+        alignment: Alignment.topCenter,
+        child: FractionallySizedBox(
+          widthFactor: 4 / 5,
+          heightFactor: 3 / 5,
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: paddingHorizontal(context),
+                vertical: paddingVertical(context)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                  fit: FlexFit.tight,
+                  flex: 3,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [Image.asset('assets/images/flower.png')]),
+                ),
+                Flexible(
+                  fit: FlexFit.tight,
+                  flex: 2,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [Text('Keane')]),
+                ),
+                Flexible(
+                  fit: FlexFit.tight,
+                  flex: 1,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [Text('The Best Kitty')]),
+                ),
+                Flexible(
+                  fit: FlexFit.tight,
+                  flex: 1,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [Text('555 555 5555')]),
+                ),
+                Flexible(
+                  fit: FlexFit.tight,
+                  flex: 1,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [Text('website 1'), Text('website deux')]),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //     child: Text('Business Card'),
+  //   );
+  // }
 }
